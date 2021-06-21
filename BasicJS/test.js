81828be3bd28c066b5f0493b375dea80b3230e5d
ValentinFilipov <valentinmf@gmail.com>
2021-06-21 16:21:16 +0300

    Update test.js

--- BasicJS/test.js
+++ BasicJS/test.js
@@ -1,9 +1,10 @@
-function print1to10 (input) {
+function print1to10(input) {
 
-    for (let i = 1; i <= input; i++) {
-        console.log(i);
-        
+    // let Array = input;
+    for (let i = 0; i < input.length; i++) {
+        console.log(`Result: ${input[i]};`);
     }
+
 }
 
-print1to10(10);
+print1to10([1, "tt", 3, 4, 5, 6, 7, 8, 9]);
